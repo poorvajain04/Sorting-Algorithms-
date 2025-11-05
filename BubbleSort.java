572c5b791403f81b0076014a807cd37502024541
Poorva Jain <poorvajain55@gmail.com>
2025-11-05 22:14:06 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums={-31,-89,0,56,12,-9,50,12,77,-30,90,-9,2,9,75,32};
+        bubSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void bubSort(int[]arr){
+        boolean swapped;
+        for(int i=0;i<arr.length;i++){
+            swapped=false;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}

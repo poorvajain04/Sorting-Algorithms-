bc9c7e05db749dd53d2a7cd1acd04eb9b6c303d8
Poorva Jain <poorvajain55@gmail.com>
2025-11-11 22:21:35 +0530

    Add files via upload

---
+++ firstPositiveNumber.java
@@ -0,0 +1,29 @@
+public class firstPositiveNumber {
+    public static void main(String[] args) {
+        int[] arr={7,8,9,11,12};
+        System.out.println(firstMissingPositive(arr));
+    }
+    public static int firstMissingPositive(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]<=nums.length && nums[i]>0 && nums[i]!=nums[correct]){
+                swap(nums,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return nums.length+1;
+    }
+    public static void swap(int[]nums,int first,int second){
+        int temp=nums [first];
+        nums[first]=nums[second];
+        nums[second]=temp;
+    }
+}
